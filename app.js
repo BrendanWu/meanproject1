5bbfd6ff617f6215d009e8070bc3a6c802d399d4
BrendanWu <wubrendan7@gmail.com>
2018-05-02 01:28:00 -0400

    3

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var express = require("express");
 var app = express();
 var path = require('path');
 
+var routes = require("./routes");
+
 app.set('port',3000);
-
-
-app.get("/", function(req,res){
-    console.log("GET the homepage");
-    res
-        .status(200)
-        .sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use('/css',function(req,res,next){
+    console.log(req.method,req.url)
+    next();
 });
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/api",routes);
 
 app.get("/json", function(req,res){
     console.log("GET the json");
